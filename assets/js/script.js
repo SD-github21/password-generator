d51b035516c2ba20bfe24c977733013949cab929
SD-github21 <sapandonde.gmail.com>
2022-04-01 23:30:47 -0500

    try to add characters through an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Assignment code here
 
-// Create password length prompt with conditional recursive function call to ensure password falls only between 8-128 characters
+// Create password length prompt 
+// Add conditional recursive function call to ensure password falls only between 8-128 characters
 var checkLength = function () {
   var passwordLength = window.prompt("Please choose the length of your password (between 8 and 128 characters)");
     if (passwordLength <8 || passwordLength > 128) {
@@ -10,16 +11,9 @@ var checkLength = function () {
 }
 checkLength();
 
-// add prompts to confirm inclusion of different character types in password generator
 
-// add variables to hold character types -- using arrays
-// var alphaLower = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x,", "y", "z"]
-// var alphaUpper = ["A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X,", "Y", "Z"]
-// var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// var specialChar = ["!", "#", "$", "%", "&", "*", "+", "/", ":", ";", "<", "=", ">", "?", "@", "^", "{", "|", "}", "~"]
-
-// add variables to hold character types -- using "charAt" method
-
+// Add prompts to confirm inclusion of different character types in password generator 
+// Validate that at least one character has been chosen
 var charTypeCheck = function() {
   var lowerCaseConfirm = window.confirm("Do you want to include lowercase characters in your password?");
   var upperCaseConfirm = window.confirm("Do you want to include uppercase characters in your password?");
@@ -34,6 +28,23 @@ var charTypeCheck = function() {
 
 charTypeCheck();
 
+// build generatePassword function using arrays
+var generatePassword = function() {
+  var password = [];
+  var alphaLower = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x,", "y", "z"]
+  var alphaUpper = ["A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X,", "Y", "Z"]
+  var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  var specialChar = ["!", "#", "$", "%", "&", "*", "+", "/", ":", ";", "<", "=", ">", "?", "@", "^", "{", "|", "}", "~"]
+
+  for (var i = 0; i <= passwordLength + 1; i++) {
+    if ((lowerCaseConfirm) && (upperCaseConfirm) && (numericConfirm) && (specialCharConfirm)) {
+      password += alphaLower
+
+  }
+}
+
+
+// add variables to hold character types -- using characters
 
 // var charTypeCheck = function() {
 //   if ((lowerCaseConfirm) && (upperCaseConfirm) && (numericConfirm) && (specialCharConfirm)) {
@@ -99,14 +110,6 @@ charTypeCheck();
 // }
 
 // charTypeCheck();
-
-// var generatePassword = function() {
-//   var password = "";
-//   for (var i = 0; i <= passwordLength; i++) {
-//       password = 
-
-//   }
-// }
 
 
 
