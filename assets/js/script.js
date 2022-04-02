c3040c6b65e55d1f9fe2a8e37aeb1ee19df7be39
SD-github21 <sapandonde.gmail.com>
2022-04-01 22:39:34 -0500

    add character types without arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,13 +16,17 @@ var upperCaseConfirm = window.confirm("Do you want to include uppercase characte
 var numericConfirm = window.confirm("Do you want to include numeric characters in your password?");
 var specialCharConfirm = window.confirm("Do you want to include special characters in your password?");
 
-// add variables to hold character types
-var alphaLower = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x,", "y", "z"]
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X,", "Y", "Z"]
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var specialChar = ["!", "#", "$", "%", "&", "*", "+", "/", ":", ";", "<", "=", ">", "?", "@", "^", "{", "|", "}", "~"]
+// add variables to hold character types -- using arrays
+// var alphaLower = ["a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x,", "y", "z"]
+// var alphaUpper = ["A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X,", "Y", "Z"]
+// var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// var specialChar = ["!", "#", "$", "%", "&", "*", "+", "/", ":", ";", "<", "=", ">", "?", "@", "^", "{", "|", "}", "~"]
 
-
+// add variables to hold character types -- using "charAt" method
+var alphaLower = "abcdefghijklmnopqrstuvwxyz"
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "0123456789"
+var specialChar = "!#$%&*+,/:;<=>?@^{|}~"
 
 
 
